3fc8c991155420c741c5143190e089f75b49bc40
Kaitlin Doubletein <kdoubles@csrwks2021-0114.engin.umich.edu>
2023-05-30 13:47:54 -0400

    adding code to process mf shell output files.

---
+++ plotting_codes/mf_shell_plotting_code.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 09:38:56 2023
+
+@author: kdoubles
+"""
+
+'''
+Proto code for multifluid outputs from SWMF.
+Based off of ShellSlice class from Spacepy.
+'''
+
+import numpy as np
+import spacepy
+from spacepy import pybats
+from spacepy.pybats import bats, rim, IdlFile, calc_wrapper
+import spacepy.datamodel as dm
+from spacepy.plot import set_target, applySmartTimeTicks
+from matplotlib.gridspec import GridSpec
+from matplotlib import gridspec
+import matplotlib.pyplot as plt
+import argparse as ap
+import os, glob
+
+class ShellSlice(IdlFile):
+    
+    def __init__(self, filename, format='binary', *args, **kwargs):
+
+        from spacepy.pybats import parse_filename_time
+
+        IdlFile.__init__(self, filename, format=format, keep_case=False)
+
+        # Extract time from file name:
+        i_iter, runtime, time = parse_filename_time(self.attrs['file'])
+        if 'time' not in self.attrs: self.attrs['time'] = time
+        if 'iter' not in self.attrs: self.attrs['iter'] = i_iter
+
+        ### Create some helper variables for plotting and calculations
+        d2r = np.pi/180. # Convert degrees to radians.
+
+        # Get grid spacing.  If npoints ==1, set to 1 to avoid math errors.
+        self['r'] = dm.dmfilled((1,), fillval=radius, attrs={'units': 'R'})
+        self.drad = 0
+        self.dlon = (self['lon'][-1] - self['lon'][0])/max(self['grid'][0]-1,1)
+        self.dlat = (self['lat'][-1] - self['lat'][0])/max(self['grid'][1]-1,1)
+
+        self.dphi   = d2r*self.dlon
+        self.dtheta = d2r*self.dlat
+
+        # Get spherical, uniform grid in units of r_body/radians:
+        self.lon, self.r, self.lat = np.meshgrid(
+            np.array(self['lon']), np.array(self['r']), np.array(self['lat']))
+        self.phi   = d2r*self.lon
+        self.theta = d2r*(90-self.lat)
+
+        #self['x']= self.r*np.sin(self.lat)*np.cos(self.lon)
+        #self['y']= self.r*np.sin(self.lat)*np.sin(self.lon)
+        #self['z'] = self.r*np.cos(self.lat)
+        
+
+    def calc_radflux(self, var, conv=1000.*(100.0)**3):
+        '''
+        For variable *var*, calculate the radial flux of *var* through each
+        grid point in the slice as self[var]*self['ur'].
+        Resulting value stored as "var_rflx".
+        '''
+        
+        #if var+'_rflx' in self:
+        #    return
+
+        # Make sure we have radial velocity.
+        # if 'ur' not in self:
+        self.calc_urad()
+        
+        # Calc flux:
+        self[var+'_rflx'] = self[var] * self['ur'] * conv
+
+    def calc_radflu(self, var):
+        '''
+        For variable *var*, calculate the radial fluence, or the
+        spatially integrated radial flux through 2D surfaces of
+        constant radius.
+
+        Resulting variable stored as "var_rflu".  Result will be an array
+        with one value for each radial distance within the object.
+        '''
+
+        # Need at least 2D in angle space:
+        if self.dphi==0 or self.dtheta==0:
+            raise ValueError('Fluence can only be calculated for 2D+ surfaces.')
+
+        # Trim flux off of val name:
+        if '_rflx' in var: var = var[:-5]
+
+        # Convenience:
+        flux = var + '_rflx'
+        flu  = var + '_rflu'
+
+        # Make sure flux exists:
+        if flux not in self: self.calc_radflux(var)
+        
+        # Create output container, one point per radial distance:
+        self[flu] = np.zeros( self['grid'][0] )
+
+        # Integrate over all radii.
+        # Units: convert R to km and cm-3 to km.
+        for i, R in enumerate(self['r']):
+            self[flu] = np.sum(
+                (R* 6371.0)**2 * self[flux][i,:,:] * \
+                np.sin(self.theta[i,:,:])  * \
+                self.dtheta * self.dphi * 1000.**2 )
+
+    def add_cont_shell(self, value, radius, irad=0, target=None, loc=111,
+                       zlim=None, dolabel=True, add_cbar=False,
+                       dofill=True, nlev=51, colat_max=90,
+                       rotate=np.pi/2, dolog=False, direction=False, 
+                       clabel=None,latticks=15., yticksize=14, extend='both',
+                       **kwargs):
+        '''
+        For slices that cover a full hemisphere or more, create a polar
+        plot of variable *value*.
+        
+        Extra keywords are sent to the matplotlib contour command.
+        '''
+
+        from numpy import pi
+        import matplotlib.pyplot as plt
+        from matplotlib import cm
+        from matplotlib.patches import Circle
+        from matplotlib.colors import (LogNorm, Normalize, SymLogNorm)
+        from matplotlib.ticker import (LogLocator, LogFormatter,
+                                       LogFormatterMathtext, MultipleLocator)
+
+        fig, ax = set_target(target, figsize=(10,10), loc=loc, polar=True)
+        ax.set_title(f'{value} at {radius} Re')
+
+        # Isolate the values we want to plot. Convert to numpy array
+        # to avoid conflict between dmarray and contour plots.
+        # Reduce dimensionality as needed.
+        if self[value].ndim == 3:
+            z = np.array(self[value][irad, :, :])
+        else:
+            z = np.array(self[value])
+            
+        # Get max/min if none given.
+        if zlim is None:
+            zlim = [0, 0]
+            zlim[0] = z.min()
+            zlim[1] = z.max()
+            
+            if abs(zlim[0]) > zlim[1]:
+                zlim[1] = abs(zlim[0])
+
+            if abs(zlim[1]) > abs(zlim[0]):
+                zlim[0] = -zlim[1]
+            #For log space, no negative zlimits.
+            if dolog and zlim[0]<=0:
+                zlim[0] = np.min( [0.0001, zlim[1]/1000.0] )
+                
+        # Create levels and set norm based on dolog.
+        if dolog:  # Log space!
+            levs = np.power(10, np.linspace(np.log10(zlim[0]),
+                                            np.log10(zlim[1]), nlev))
+            z=np.where(z>zlim[0], z, 1.01*zlim[0])
+            norm=LogNorm()
+            ticks=LogLocator()
+            fmt=LogFormatterMathtext()
+        else:
+            levs = np.linspace(zlim[0], zlim[1], nlev)
+            norm=None
+            ticks=None
+            fmt=None
+            
+        # Select proper contour function based on fill/don't fill.
+        if dofill:
+            func = ax.contourf
+        else:
+            func = ax.contour
+
+        # Plot result.  Rotate "rotate" radians to get sun in right spot.
+        # Plot against colatitude to arrange results correctly.
+        cnt = func(self.phi[irad,:,:]+rotate, 90-self.lat[irad,:,:],
+                   np.array(z), levs, norm=norm,
+                   extend=extend, **kwargs)
+
+        # Add cbar if necessary.
+        if add_cbar:
+            cbar=plt.colorbar(cnt, ax=ax, ticks=ticks, format=fmt, shrink=.85)
+            if clabel==None:
+                clabel="{} ({})".format(value, self[value].attrs['units'])
+            cbar.set_label(clabel)
+        else:
+            cbar=None # Need to return something, even if none.
+
+        # Adjust latitude
+        ax.set_ylim( [0, colat_max] )
+
+        # Adjust atitude and add better labels:
+        ax.yaxis.set_major_locator(MultipleLocator(latticks))
+        ax.set_ylim([0,colat_max])
+        ax.set_yticklabels('')
+        opts = {'size':yticksize, 'rotation':-45, 'ha':'center', 'va':'center'}
+        for theta in np.arange(90-latticks, 90-colat_max, -latticks):
+            txt = '{:02.0f}'.format(theta)+r'$^{\circ}$'
+            ax.text(pi/4., 90.-theta, txt, color='w', weight='extra bold',**opts)
+            ax.text(pi/4., 90.-theta, txt, color='k', weight='light', **opts)
+            
+        # Use MLT-type labels.
+        lt_labels = ['Noon', '18', '00',   '06']
+        xticks    = [     0, pi/2,   pi, 3*pi/2]
+        xticks = np.array(xticks) + rotate
+        
+        # Apply x-labels:
+        ax.set_xticks(xticks)
+        ax.set_xticklabels(lt_labels)
+        
+        return fig, ax, cnt, cbar
+    
+    def arg_parse_file_location():
+        """
+        A parser that allows the user to type in the path of the file that they 
+        want to process. User must input path and file name if file is not in same
+        working directory as this script.
+            
+        Args
+        ------
+        path_name (str): 
+            Name of path to the directory of the data.
+                
+        Return
+        -------
+        Arguments for the file and the path. 
+        
+        """
+        
+        parser = ap.ArgumentParser(description = 'Read in path name')
+        parser.add_argument('-path', help = 'The path for the file. Default \
+                            directory of this script.', type = str, \
+                                default = 'None')
+        parser.add_argument('-runname', help = 'This is the name of the run. \
+                            Default is "None".', type = str, \
+                                required = True)
+        args = parser.parse_args()
+
+        return args
+    
+global file_location
+directory = ShellSlice.arg_parse_file_location()
+run_name = directory.runname
+
+if input("Is this a multifluid run? (y/n) ") != "y":
+       is_multi = False
+
+radius = '30'
+shell_file_dict = {'shell_file':[],'r_file':[],'shell_slice':[]}
+for filename in glob.glob((directory.path + '/' +'shl_mhd*.outs')):
+    shell_file_dict['shell_file'].append(filename)
+    shell_file_dict['shell_slice'].append(ShellSlice(shell_file_dict['shell_file']))
